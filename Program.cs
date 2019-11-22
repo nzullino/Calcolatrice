ea4f600c839b6d0fa7f5f1391b90389dddb64e71
nicolas <zullinon@gmail.com>
2019-11-22 16:27:50 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class HelloWorld
+{
+    public static void Main()
+    {
+        while (true) {
+            Console.WriteLine("inserisci numero 1");
+            if (Double.TryParse(Console.ReadLine().Replace(".", ","), out double n1)) {
+                string sel = "";
+                do
+                {
+                    Console.WriteLine("Inserisci numero 2");
+                    if (Double.TryParse(Console.ReadLine().Replace(".", ","), out double n2))
+                    {
+                        Console.WriteLine("inserisci segno(+ , - , * , / )oppure 'c' per cancellare il secondo numero");
+                        sel = Console.ReadLine().ToLower();
+                        if (sel == "+") Console.WriteLine($"{n1} + {n2} = {n1 += n2}");
+                        else if (sel == "-") Console.WriteLine($"{n1} - {n2} = {n1 -= n2}");
+                        else if (sel == "*") Console.WriteLine($"{n1} * {n2} = {n1 *= n2}");
+                        else if (sel == "/") Console.WriteLine($"{n1} / {n2} = {n1 /= n2}");
+                    }
+                } while (sel != "c");
+            }
+        }
+    }
+}
